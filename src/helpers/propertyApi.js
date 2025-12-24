c1414cf34cf75c93afc5261e90fbf4c719df6af1
Barsharanijena <barsharanijena555@gmail.com>
2025-12-24 18:15:39 +0530

    Update hero images for all home variations

--- src/helpers/propertyApi.js
+++ src/helpers/propertyApi.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
-const API_URL = 'http://16.16.211.219:5000/api';
-// For local testing, use:
-// const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://16.16.211.219:5000/api';
 
 const apiClient = axios.create({
   baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: false,
+  withCredentials: true,
 });
 
 const getAuthToken = () => {
